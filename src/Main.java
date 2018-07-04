2adcbf5f47e11b6c943360b82f17558746156ec7
kinya <kiernan920@gmail.com>
2018-07-04 22:55:16 +0100

    xPAth resolver

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import javax.xml.xpath.XPathConstants;
+import java.util.List;
+
+public class Main {
+
+    private static XpathExpressionResolver xpathExpressionResolver  = new XpathExpressionResolver("employees");;
+
+    public static void main(String[] args) {
+        printAllFemaleEmployees();
+        printEmployeeById(1);
+        printEmployeeAgeGreaterThan(10);
+    }
+
+    //Get all the Employee elements where gender element is female
+    private static void printAllFemaleEmployees(){
+        List<String> femaleEmployees = (List<String>) xpathExpressionResolver.resolve("//Employees/Employee[gender='Female']/name/text()", XPathConstants.NODESET);
+        System.out.println("All Female Employees: " + femaleEmployees);
+    }
+
+    //Get all the Employee elements where the id attribute value is equal to <someValue>
+    private static void printEmployeeById(int employeeId){
+        String employee = (String) xpathExpressionResolver.resolve("/Employees/Employee[@id='" + employeeId + "']/name/text()", XPathConstants.STRING);
+        System.out.println("All Employees with employId no." + employeeId + " are " + employee);
+    }
+
+    //Get all the employees where the age element is greater than <someValue>
+    private static void printEmployeeAgeGreaterThan(int employeeAge){
+        List<String> employees = (List<String>) xpathExpressionResolver.resolve("/Employees/Employee[age>" + employeeAge + "]/name/text()", XPathConstants.NODESET);
+        System.out.println("All Employees over " + employeeAge + " are "  + employees);
+    }
+}
---
+++ src/XpathExpressionResolver.java
@@ -0,0 +1,83 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.xml.namespace.QName;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+    public class XpathExpressionResolver {
+
+        private static Document xmlDocument;
+        private static XPath xPath;
+        private String xmlFileUrl;
+
+
+        public XpathExpressionResolver(String xmlFileUrl){
+            xmlDocument = createXmlDocument();
+            xPath = createXPath();
+        }
+
+
+        public Object resolve(String xPathExpression, QName qName){
+            if (qName.equals(XPathConstants.NODESET)) {
+                return getNodeValues(getXmlNodes(xPathExpression));
+            }else{
+                return getXmlString(xPathExpression);
+            }
+        }
+
+        private XPath createXPath(){
+            XPathFactory xpathFactory = XPathFactory.newInstance();
+            return xpathFactory.newXPath();
+        }
+
+        private Document createXmlDocument() {
+            DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setNamespaceAware(true);
+            try{
+                DocumentBuilder builder = factory.newDocumentBuilder();
+                return builder.parse("employees.xml");
+            }catch (ParserConfigurationException | IOException | SAXException e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        private String getXmlString(String xpathExpression){
+            try{
+                XPathExpression expr = xPath.compile(xpathExpression);
+                return (String) expr.evaluate(xmlDocument, XPathConstants.STRING);
+            } catch (XPathExpressionException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Unable to evaluate Xpath Expression");
+            }
+        }
+
+        private NodeList getXmlNodes(String xpathExpression){
+            try{
+                XPathExpression expr = xPath.compile(xpathExpression);
+                return (NodeList) expr.evaluate(xmlDocument, XPathConstants.NODESET);
+            } catch (XPathExpressionException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Unable to evaluate Xpath Expression");
+            }
+        }
+
+        private List<String> getNodeValues(NodeList nodeList){
+            List nodeValueList = new ArrayList();
+            for (int i = 0; i < nodeList.getLength(); i++)
+                nodeValueList.add(nodeList.item(i).getNodeValue());
+            return nodeValueList;
+        }
+    }
